fc43b79da8d346ca3c9c3cd0f4227a467629fd06
Emil Petersson <emil.petersson@live.com>
2017-04-23 12:46:02 +0200

    Added Journal as Boostrap theme for UI

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace IntelligentMonitoringAPI
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-journal.css",
                       "~/Content/site.css"));
         }
     }
