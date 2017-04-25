2bc70ccf5888996170a4302fcf1513cda3c802db
Emil Petersson <emil.petersson@live.com>
2017-04-25 21:46:03 +0200

    Sensors have CRUD functionality again after Db change

--- IntelligentMonitoringAPI/Controllers/SensorsController.cs
+++ IntelligentMonitoringAPI/Controllers/SensorsController.cs
@@ -10,7 +10,7 @@ using IntelligentMonitoringAPI.Models.DTOs;
 
 namespace IntelligentMonitoringAPI.Controllers
 {
-    /*public class SensorsController : ApiController
+    public class SensorsController : ApiController
     {
         private IntelliMonDbContext _context;
 
@@ -27,18 +27,19 @@ namespace IntelligentMonitoringAPI.Controllers
 
             return Ok(sensorsDto);
         }
-
+        
         [HttpGet]
-        public IHttpActionResult GetSensor(string id)
+        public IHttpActionResult GetSensor(int id)
         {
-            var sensor = _context.Sensors.SingleOrDefault(c => c.ExternalId == id);
+            var sensor = _context.Sensors.SingleOrDefault(c => c.Id == id);
 
             if (sensor == null)
                 return NotFound();
 
             return Ok(Mapper.Map<Sensor, SensorDto>(sensor));
         }
-
+        
+        
         [HttpPost]
         public IHttpActionResult CreateSensor(SensorDto sensorDto)
         {
@@ -50,26 +51,34 @@ namespace IntelligentMonitoringAPI.Controllers
             _context.Sensors.Add(sensor);
             _context.SaveChanges();
 
-            return Created(new Uri(Request.RequestUri + "/" + sensor.ExternalId), sensorDto);
+            sensorDto.Id = sensor.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + sensor.Id), sensorDto);
         }
 
+        
         [HttpPut]
-        public IHttpActionResult UpdateSensor(string id, SensorDto sensorDto)
+        public IHttpActionResult UpdateSensor(int id, SensorDto sensorDto)
         {
-            var sensorInDb = _context.Sensors.SingleOrDefault(c => c.ExternalId == id);
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var sensorInDb = _context.Sensors.SingleOrDefault(c => c.Id == id);
+
             if (sensorInDb == null)
                 return NotFound();
 
             Mapper.Map(sensorDto, sensorInDb);
+
             _context.SaveChanges();
 
             return Ok();
         }
-
+        
         [HttpDelete]
-        public IHttpActionResult DeleteSensor(string id)
+        public IHttpActionResult DeleteSensor(int id)
         {
-            var device = _context.Sensors.SingleOrDefault(c => c.ExternalId == id);
+            var device = _context.Sensors.SingleOrDefault(c => c.Id == id);
             if (device == null)
                 return NotFound();
 
@@ -79,5 +88,5 @@ namespace IntelligentMonitoringAPI.Controllers
             return Ok();
         }
         
-    }*/
+    }
 }
