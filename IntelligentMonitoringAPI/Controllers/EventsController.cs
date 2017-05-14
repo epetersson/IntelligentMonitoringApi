1f1492e9b48be84b67d09d7d63a2fcc48e2a7518
Emil Petersson <emil.petersson@live.com>
2017-05-14 22:12:33 +0200

    Added simple routes for getting events. Will need further work.

---
+++ IntelligentMonitoringAPI/Controllers/EventsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using IntelligentMonitoringAPI.Models;
+using IntelligentMonitoringAPI.Models.Wrappers;
+using IntelligentMonitoringBackend.ModelsDTO;
+
+namespace IntelligentMonitoringAPI.Controllers
+{
+    public class EventsController : ApiController
+    {
+        private IntelliMonDbContext _context;
+
+        public EventsController()
+        {
+            _context = new IntelliMonDbContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetEvents()
+        {
+            var eventDtos = _context.Events
+                .ToList()
+                .Select(Mapper.Map<Event, EventDTO>);
+
+            var response = new EventsWrapper { Events = eventDtos };
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("api/events/device/{deviceId}")]
+        public IHttpActionResult GetEventsForDevice(string deviceId)
+        {
+            var deviceEventDtos =
+                _context.Events.ToList()
+                .Where(c => c.DeviceId == deviceId)
+                .Select(Mapper.Map<Event, EventDTO>);
+
+            var response = new EventsWrapper {Events = deviceEventDtos};
+
+            return Ok(response);
+        }
+    }
+}
