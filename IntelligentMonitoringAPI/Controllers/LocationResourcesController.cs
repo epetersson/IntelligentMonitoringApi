30d560dd258b55403eb7e7c66e6ab8c20bcbbe4e
Emil Petersson <emil.petersson@live.com>
2017-05-14 22:12:57 +0200

    Added simple routes for getting locationResources. Will need further work.

---
+++ IntelligentMonitoringAPI/Controllers/LocationResourcesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using IntelligentMonitoringAPI.Models;
+using IntelligentMonitoringAPI.Models.DTOs;
+using IntelligentMonitoringAPI.Models.Wrappers;
+
+namespace IntelligentMonitoringAPI.Controllers
+{
+    public class LocationResourcesController : ApiController
+    {
+        private IntelliMonDbContext _context;
+
+        public LocationResourcesController()
+        {
+            _context = new IntelliMonDbContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetLocationResources()
+        {
+            var locationResourceDtos = _context.LocationResources
+                .ToList()
+                .Select(Mapper.Map<LocationResource, LocationResourceDto>);
+
+            var response = new LocationResourcesWrapper {LocationResources = locationResourceDtos};
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetLocationResource(string id)
+        {
+            var locationResource = _context.LocationResources.SingleOrDefault(c => c.Id == id);
+
+            if (locationResource == null)
+                return NotFound();
+
+            var locationResourceDto = Mapper.Map<LocationResource, LocationResourceDto>(locationResource);
+
+            var response = new LocationResourceWrapper {LocationResource = locationResourceDto};
+
+            return Ok(response);
+        }
+
+
+    }
+}
