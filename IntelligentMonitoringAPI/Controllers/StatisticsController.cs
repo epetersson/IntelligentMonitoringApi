938f7b20cba0b49b49f198ae8cc98e26075f7d3e
Emil Petersson <emil.petersson@live.com>
2017-05-22 19:06:15 +0200

    Renamed DeviceHistoriesController to StatisticsController

---
+++ IntelligentMonitoringAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using IntelligentMonitoringAPI.Models;
+using IntelligentMonitoringAPI.Models.DTOs;
+using IntelligentMonitoringAPI.Models.Wrappers;
+
+namespace IntelligentMonitoringAPI.Controllers
+{
+    /// <summary>
+    /// Class defines the endpoints related to DailyStatistics and HourlyStatistics.
+    /// </summary>
+    public class StatisticsController : ApiController
+    {
+        private IntelliMonDbContext _context;
+
+        public StatisticsController()
+        {
+            _context = new IntelliMonDbContext();
+        }
+
+        /// <summary>
+        /// Method defines endpoint for getting all DailyStatistics.
+        /// </summary>
+        /// <returns>Array of DailyStatisticsDtos wrapped in JSON-object</returns>
+        [HttpGet]
+        public IHttpActionResult GetDailyStatistics()
+        {
+            var dailyStatisticDtos = _context.DailyStatistics.ToList()
+                .Select(Mapper.Map<DailyStatistic, DailyStatisticDto>);
+
+            var response = new DailyStatisticsWrapper() { DailyStatistics = dailyStatisticDtos };
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Method defines endpoint for getting all DeviceHistories for specified device.
+        /// </summary>
+        /// <param name="deviceId">string</param>
+        /// <returns>Array of DeviceHistoryDtos wrapped in JSON-object</returns>
+        [HttpGet]
+        [Route("api/Statistics/{deviceId}/hourly")]
+        public IHttpActionResult GetDeviceHistoryByDeviceId(string deviceId)
+        {
+            var deviceHistoryDtos =
+                _context.DeviceHistories.ToList()
+                    .Where(c => c.DeviceId == deviceId)
+                    .Select(Mapper.Map<DeviceHistory, DeviceHistoryDto>);
+
+            var response = new DeviceHistoriesWrapper {DeviceHistories = deviceHistoryDtos};
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Method defines endpoint for getting all DailyStatistics for defined Device.
+        /// </summary>
+        /// <param name="deviceId">string</param>
+        /// <returns>Array of DailyStatisticDtos wrapped in JSON-object</returns>
+        [HttpGet]
+        [Route("api/Statistics/{deviceId}/daily")]
+        public IHttpActionResult GetDailyStatistics(string deviceId)
+        {
+            var dailyStatisticsDto = _context.DailyStatistics
+                .ToList()
+                .Where(c => c.DeviceId == deviceId)
+                .Select(Mapper.Map<DailyStatistic, DailyStatisticDto>);
+
+            var response = new DailyStatisticsWrapper {DailyStatistics = dailyStatisticsDto};
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Method defines endpoint for getting all DailyStatistics within a timespan for a device. 
+        /// </summary>
+        /// <param name="deviceId">string</param>
+        /// <param name="startDate">DateTime</param>
+        /// <param name="endDate">DateTime</param>
+        /// <returns>Array of DailyStatisticDtos wrapped in JSON-object</returns>
+        [HttpGet]
+        public IHttpActionResult GetDailyStatisticForDeviceByDateToDate(string deviceId, DateTime startDate, DateTime endDate)
+        {
+            var endingDate = endDate.AddDays(1);
+
+            var dailyStatisticDtos = _context.DailyStatistics.ToList()
+                .Where(c => c.DeviceId == deviceId)
+                .Where(c => c.TimeStamp >= startDate && c.TimeStamp <= endingDate)
+                .Select(Mapper.Map<DailyStatistic, DailyStatisticDto>);
+
+            var response = new DailyStatisticsWrapper { DailyStatistics = dailyStatisticDtos };
+
+            return Ok(response);
+        }
+    }
+}
