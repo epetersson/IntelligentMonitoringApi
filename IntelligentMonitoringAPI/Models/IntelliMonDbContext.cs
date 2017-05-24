b0ac0c0b5937c52c3ec35df0984dca96cfaef97e
Emil Petersson <emil.petersson@live.com>
2017-05-24 22:22:03 +0200

    Added RealTimeToken, renamed DeviceHistory to HourlyStatistics and removed monthly, weekly and yearlystatistics

--- IntelligentMonitoringAPI/Models/IntelliMonDbContext.cs
+++ IntelligentMonitoringAPI/Models/IntelliMonDbContext.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
 namespace IntelligentMonitoringAPI.Models
 {
-    using System;
-    using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
-
-    public partial class IntelliMonDbContext : DbContext
+    public class IntelliMonDbContext : DbContext
     {
         public IntelliMonDbContext()
             : base("name=IntelliMonDbContext")
@@ -13,23 +13,35 @@ namespace IntelligentMonitoringAPI.Models
         }
         
         public DbSet<Device> Devices { get; set; }
+
         public DbSet<Event> Events { get; set; }
+
         public DbSet<Location> Locations { get; set; }
+
         public DbSet<DeviceNetwork> DeviceNetworks { get; set; }
+
         public DbSet<Sensor> Sensors { get; set; }
+
         public DbSet<LocationResource> LocationResources { get; set; }
+
         public DbSet<Position> Positions { get; set; }
+
         public DbSet<SensorMeasurement> SensorMeasurements { get; set; }
+
         public DbSet<DataCollector> DataCollectors { get; set; }
-        public DbSet<DeviceHistory> DeviceHistories { get; set; }
+
+        public DbSet<HourlyStatistic> HourlyStatistics { get; set; }
+
         public DbSet<DailyStatistic> DailyStatistics { get; set; }
-        public DbSet<MonthlyStatistic> MonthlyStatistics { get; set; }
-        public DbSet<WeeklyStatistic> WeeklyStatistics { get; set; }
-        public DbSet<YearlyStatistic> YearlyStatistics { get; set; }
+
         public DbSet<UserConversation> UserConversations { get; set; }
 
+        public DbSet<RealTimeToken> RealTimeTokens { get; set; }
+
         public DbSet<AccumulatedStatistic> AccumulatedStatistics { get; set; }
+
         public DbSet<CustomEvent> CustomEvents { get; set; }
+
         public DbSet<FailurePronedDevice> FailurePronedDevices { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
