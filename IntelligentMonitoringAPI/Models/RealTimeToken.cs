bc9e7d5f6e8710d691e1e501e9d98608cfd3b9de
Emil Petersson <emil.petersson@live.com>
2017-05-24 21:59:22 +0200

    Added RealTimeToken to db

---
+++ IntelligentMonitoringAPI/Models/RealTimeToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntelligentMonitoringAPI.Models
+{
+    public class RealTimeToken
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string DeviceNetworkId { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime? CreatedTimeStamp { get; set; }
+
+        public DateTime? ExpiresTimeStamp { get; set; }
+    }
+}
