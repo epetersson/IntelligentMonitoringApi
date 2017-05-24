8e678558d0f51d88898fd9903b442ee3becc1765
Emil Petersson <emil.petersson@live.com>
2017-05-24 22:45:00 +0200

    Refactoring

--- IntelligentMonitoringAPI/Models/UserConversation.cs
+++ IntelligentMonitoringAPI/Models/UserConversation.cs
@@ -1,10 +1,3 @@
-using Microsoft.Bot.Connector;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-
 namespace IntelligentMonitoringAPI.Models
 {
     public class  UserConversation
